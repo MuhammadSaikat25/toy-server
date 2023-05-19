e2283c470aad3f28bbd0e1944f4827764fda3f2a
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-05-19 20:20:50 +0600

    delete a single data

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express=require('express')
-const cors=require('cors')
-const app=express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const app = express()
 app.use(express.json())
 require('dotenv').config()
 app.use(cors())
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2a0jwzs.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get('/',(req,res)=>{
-    res.send('toy shop')
+app.get('/', (req, res) => {
+  res.send('toy shop')
 })
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,34 +24,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful 
-    const createToy=client.db('Toy-Hub').collection('add-toy')
-   
-    app.post('/AddToy',async(req,res)=>{
-      const body=req.body 
-      const result=await createToy.insertOne(body)
+    const createToy = client.db('Toy-Hub').collection('add-toy')
+
+    app.post('/AddToy', async (req, res) => {
+      const body = req.body
+      const result = await createToy.insertOne(body)
       res.send(result)
     })
+
     // get data by age
-    app.get('/getToy/:age',async(req,res)=>{
-      const data=req.params.age
-      const age={age:data}
-      const result=await createToy.find(age).toArray()
+    app.get('/getToy/:age', async (req, res) => {
+      const data = req.params.age
+      const age = { age: data }
+      const result = await createToy.find(age).toArray()
       res.send(result)
-     
+
     })
+
+    // get data by id
+    app.get('/getById/:id', async (req, res) => {
+      const id = req.params.id
+
+      const data = { _id: new ObjectId(id) }
+      const result = await createToy.findOne(data)
+      res.send(result)
+    })
+
     // get data by email
-    app.get('/myToy',async(req,res)=>{
-      let query={}
-      if(req.query?.email){
-        query={email:req.query.email}
+    app.get('/myToy', async (req, res) => {
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
-      const result=await createToy.find(query).toArray()
+      const result = await createToy.find(query).toArray()
       res.send(result)
     })
-    app.get('/allToy',async(req,res)=>{
-      const result=await createToy.find().toArray()
+
+    // get all toy
+    app.get('/allToy', async (req, res) => {
+      const result = await createToy.find().toArray()
+      res.send(result)
+    })
+
+    // delete any data
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id
+      const data = { _id: new ObjectId(id) }
+      const result = await createToy.deleteOne(data)
       res.send(result)
     })
     await client.db("admin").command({ ping: 1 });
